dcfa323952be3de7bbb602660dbb6c5f2043fad4
duonghuupha <duonghuupha@hotmail.com.vn>
2023-08-03 02:13:38 +0700

    sadf

--- libs/config/paths.php
+++ libs/config/paths.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 define('URL', 'http://'.$_SERVER['HTTP_HOST']);
-define('URL_IMAGE', 'http://portalnaturem:81/public/images');
+define('URL_IMAGE', 'http://images.naturem.us');
 $dirtionary = realpath($_SERVER['DOCUMENT_ROOT']); 
-$dirtionary = str_replace("naturem", "portalnaturem", $dirtionary);
+$dirtionary = str_replace("naturem", "portal_naturem", $dirtionary);
 define('DIR_IMAGE', $dirtionary.'/public/images');
 define('DIR', $_SERVER['DOCUMENT_ROOT']);
 ?>
